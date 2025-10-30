7fc1bf0702dae6ab108e8b0d7c572868b377d541
bidintel-app <kay.thomas@live.com>
2025-10-30 15:37:03 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import PricingPage from './components/PricingPage';
+import OpportunitiesPage from './components/OpportunitiesPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/pricing" element={<PricingPage />} />
+          <Route path="/opportunities" element={<OpportunitiesPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
